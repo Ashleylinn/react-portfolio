12ece8ceb98891be25a5222a6bbe8defde33a975
Ashleylinn <ashleylin1016@gmail.com>
2025-06-01 19:33:52 -0400

    update portfolio

---
+++ src/about/subheadingsData.js
@@ -0,0 +1,49 @@
+const subheadingsData = {
+    1: [
+        {
+            title: "About Me",
+            content: (
+                <>
+                    <p>
+                    With a background in Information Technology and Business, 
+                    I thrive at the intersection of data, strategy, and innovation. 
+                    I have experience in marketing, data analysis, and frontend development, 
+                    with an interest in UI/UX design. I enjoy creating intuitive and engaging user experiences while ensuring seamless interactions between users and technology.
+                    </p>
+                </>
+            ),
+        },
+    ],
+    2: [
+        {
+            title: "2025 Bachelor Degree",
+            content: (
+                <>
+                    <p>
+                        University of Toronto
+                    </p>
+                    <p>
+                        Specialization in Management and Information Technology
+                    </p>
+                </>
+            ),
+        },
+    ],
+    3: [
+        {
+            title: "Creative Technology",
+            content: (
+                <>
+                    <p>
+                    I'm fascinated by how people interact with technology—what makes an interface intuitive, what builds trust, and how design 
+                    influences behavior. I love diving into UI/UX principles, experimenting with layouts, and observing how small changes affect user 
+                    engagement. 
+                    </p>
+                </>
+            ),
+        },
+    ],
+
+};
+
+export default subheadingsData;
---
+++ src/skills/skillsData.js
@@ -0,0 +1,69 @@
+const skills = {
+    1: [
+        {
+            title: "HTML",
+            level: 5,
+        },
+        {
+            title: "CSS",
+            level: 5,
+        },
+        {
+            title: "React JS",
+            level: 4,
+        },
+        {
+            title: "JavaScript",
+            level: 4,
+        },
+        {
+            title: "TypeScript",
+            level: 2,
+        },
+        {
+            title: "Figma",
+            level: 6,
+        },
+        {
+            title: "Material UI",
+            level: 6,
+        },
+        {
+            title: "XML",
+            level: 5,
+        },
+    ],
+    2: [
+        {
+            title: "PostgreSQL",
+            level: 5,
+        },
+        {
+            title: "Node.js",
+            level: 4,
+        },
+        {
+            title: "MongoDB",
+            level: 3,
+        },
+        {
+            title: "Git",
+            level: 4,
+        },
+        {
+            title: "Java",
+            level: 3,
+        },
+        {
+            title: "SQL",
+            level: 5,
+        },
+        {
+            title: "Python",
+            level: 4,
+        },
+    ],
+};
+  
+export default skills;
+  
